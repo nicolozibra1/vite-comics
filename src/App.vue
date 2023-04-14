3daf7ed3689c5f00f7786637eacf1e5a9923c13e
nicolozibra1 <nico.zibra@gmail.com>
2023-04-14 15:57:06 +0200

    add-MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
   export default {
     name: 'App',
     components: {
-      HeaderComponent
+      HeaderComponent,
+      MainComponent
     }
   }
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <main>
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+            -- Content goes here --
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    main{
+        background-color: #1c1c1c;
+        color: white;
+        height: 150px;
+        font-size: 2rem;
+    }
+</style>
