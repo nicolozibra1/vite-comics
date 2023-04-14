3212a11ae20f476b1ed39a7721cdc40984c3af77
nicolozibra1 <nico.zibra@gmail.com>
2023-04-14 17:08:06 +0200

    add-bannerComponent (fix-dimension-logo-required)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <HeaderComponent />
   <MainComponent />
+  <BannerComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import BannerComponent from './components/BannerComponent.vue';
   export default {
     name: 'App',
     components: {
       HeaderComponent,
-      MainComponent
+      MainComponent,
+      BannerComponent
     }
   }
 </script>
---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="banner">
+        <div class="container h-100">
+            <ul v-for="(item, index) in items" :key="index" class="list-unstyled d-flex">
+                <li>
+                    <a href="#" class="d-flex justify-content-center align-items-center">
+                        <img :src="'img/' + item.image" alt="">
+                        <span>{{ item.title }}</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import {bannerPromo} from '../data/data.js'
+    export default {
+        name: 'BannerComponent',
+        data() {
+            return {
+                items: bannerPromo
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+#banner{
+    background-color: #0281f9;
+    height: 200px;
+
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center
+    }
+
+    ul{
+        max-width: 300px;
+    }
+
+    a{
+        color:white;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: 400;
+        font-size: 0.9rem;
+
+        img{
+            width: 25%;
+        }
+    }
+}
+</style>
