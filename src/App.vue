9d0cb7d5fa4a6a2607db0cb7e0f90abccd6d31ff
nicolozibra1 <nico.zibra@gmail.com>
2023-04-14 15:48:12 +0200

    add-HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent />
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
   export default {
-    
+    name: 'App',
+    components: {
+      HeaderComponent
+    }
   }
 </script>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="container d-flex justify-content-between align-items-center p-3">
+        <div class="box-logo w-50">
+            <img src="../assets/img/dc-logo.png" alt="dc logo">
+        </div>
+
+        <ul v-for="(link, index) in links" :key="index" class="list-unstyled d-flex">
+            <li class="px-3">
+                <a href="#">{{ link.title }}</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+import {navLinks} from '../data/data.js'
+    export default {
+        name: 'HeaderComponent',
+        data() {
+            return {
+                links: navLinks
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 120px;
+}
+
+ul{
+    li a{
+        text-decoration: none;
+        color: black;
+        text-transform: uppercase;
+        font-family: 'Roboto','sans-serif';
+        font-weight: 600;
+        font-size: 0.9rem;
+    }
+}
+</style>
