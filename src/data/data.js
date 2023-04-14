ddb2d05c0eb5fbb56711a86d10c8e76f1c1a876b
nicolozibra1 <nico.zibra@gmail.com>
2023-04-14 15:18:46 +0200

    create-data.js

---
+++ src/data/data.js
@@ -0,0 +1,194 @@
+const navLinks = [
+    {
+        title: 'characters',
+        active: false,
+    },
+    {
+        title: 'comics',
+        active: true,
+    },
+    {
+        title: 'movies',
+        active: false,
+    },
+    {
+        title: 'tv',
+        active: false,
+    },
+    {
+        title: 'games',
+        active: false,
+    },
+    {
+        title: 'collectibles',
+        active: false,
+    },
+    {
+        title: 'videos',
+        active: false,
+    },
+    {
+        title: 'fans',
+        active: false,
+    },
+    {
+        title: 'news',
+        active: false,
+    },
+    {
+        title: 'shop',
+        active: false,
+    }
+]
+
+const bannerPromo = [
+    {
+        title: 'digital comics',
+        image: 'buy-comics-digital-comics.png'
+    },
+    {
+        title: 'dc merchandise',
+        image: 'buy-comics-merchandise.png'
+    },
+    {
+        title: 'subscription',
+        image: 'buy-comics-subscriptions.png'
+    },
+    {
+        title: 'comic shop locator',
+        image: 'buy-comics-shop-locator.png'
+    },
+    {
+        title: 'dc power visa',
+        image: 'buy-dc-power-visa.svg'
+    }
+]
+
+const footerLinks = [
+    {
+        id: 1,
+        title: 'dc comics',
+        links: [
+            {
+                text: 'characters',
+                link: '#'
+            },
+            {
+                text: 'comics',
+                link: '#'
+            },
+            {
+                text: 'movies',
+                link: '#'
+            },
+            {
+                text: 'tv',
+                link: '#'
+            },
+            {
+                text: 'games',
+                link: '#'
+            },
+            {
+                text: 'videos',
+                link: '#'
+            },
+            {
+                text: 'news',
+                link: '#'
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: 'shop',
+        links: [
+            {
+                text: 'Shop DC',
+                link: '#'
+            },
+            {
+                text: 'Shop DC Collectibles',
+                link: '#'
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'dc',
+        links: [
+            {
+                text: 'terms of use',
+                link: '#'
+            },
+            {
+                text: 'privacy poolicy (new)',
+                link: '#'
+            },
+            {
+                text: 'ad choices',
+                link: '#'
+            },
+            {
+                text: 'advertising',
+                link: '#'
+            },
+            {
+                text: 'jobs',
+                link: '#'
+            },
+            {
+                text: 'subscriptions',
+                link: '#'
+            },
+            {
+                text: 'talent workshops',
+                link: '#'
+            },
+            {
+                text: 'CPSC Certificates',
+                link: '#'
+            },
+            {
+                text: 'ratings',
+                link: '#'
+            },
+            {
+                text: 'Show help',
+                link: '#'
+            },
+            {
+                text: 'contact us',
+                link: '#'
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: 'sites',
+        links: [
+            {
+                text: 'DC',
+                link: '#'
+            },
+            {
+                text: 'MAD Magazine',
+                link: '#'
+            },
+            {
+                text: 'DC Kids',
+                link: '#'
+            },
+            {
+                text: 'DC Universe',
+                link: '#'
+            },
+            {
+                text: 'DC Power Visa',
+                link: '#'
+            }
+        ]
+    },
+]
+
+export {navLinks, bannerPromo, footerLinks}
